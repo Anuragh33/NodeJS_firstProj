e1a4d45516b3c0c70c0b41bdf723dd64991d0d6e
Ragidimilli Anuragh <Princeanuragh1@gmail.com>
2024-03-23 21:35:33 -0500

    updtd

--- starter/index.js
+++ starter/index.js
@@ -25,24 +25,68 @@ const url = require("url")
 // })
 
 /////////////////////////////////////////
-//server
+//SERVER
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+
+  output = temp.replace(/{%IMAGE%}/g, product.image)
+  output = temp.replace(/{%PRICE%}/g, product.price)
+  output = temp.replace(/{%FROM%}/g, product.from)
+  output = temp.replace(/{%NUTRIENTS%}/g, product.nutrients)
+  output = temp.replace(/{%ID%}/g, product.id)
+  output = temp.replace(/{%DESCRIPTION%}/g, product.description)
+  output = temp.replace(/{%QUNATITY%}/g, product.quantity)
+
+  if (!product.organic)
+    output = temp.replace(/{%NOT_Organic%}/g, "not - organic")
+
+  return output
+}
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8")
+
+const templateOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+)
+const templateCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+)
+
+const templateProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+)
+
+const dataObj = JSON.parse(data)
 
 const server = http.createServer((req, res) => {
   const pathName = req.url
   if (pathName === "/" || pathName === "/overview") {
-    res.end("Hello, this is OVERVIEW!")
-  } else if (pathName === "/api") {
-    fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
-      const productName = JSON.parse(data)
-      res.writeHead(200, { "Content-type": "application/json" })
-      res.end(data)
-    })
-  } else if (pathName === "/product") {
-    res.end("Hello, this is PRODUCT!")
-  } else {
-    res.writeHead(404, {
-      "Content-type": "text/html",
-    })
+    res.writeHead(200, { "Content-type": "text/html" })
+
+    const cardsHTML = dataObj.map((el) => replaceTemplate(templateCard, el))
+
+    res.end(templateOverview)
+  }
+
+  ///////////////////////////////////////////////////
+  else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" })
+    res.end(data)
+  }
+
+  ///////////////////////////////////////////////////
+  else if (pathName === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" })
+    res.end(templateProduct)
+  }
+
+  ///////////////////////////////////////////////////
+  else {
+    res.writeHead(404, { "Content-type": "text/html" })
     res.end("<h1>The page cannot be found.</h1>")
   }
 })
